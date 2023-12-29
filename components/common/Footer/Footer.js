9a192bbf8708037a04debc4ff634a3a0786c5a43
saranshsinhaa <saranshsinha084@gmail.com>
2023-12-29 23:55:41 +0530

    first commit

---
+++ components/common/Footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-gray-600">This is the footer.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/teams.js
@@ -0,0 +1,19 @@
+import Navbar from "../components/common/Navbar/Navbar";
+import Footer from "../components/common/Footer/Footer";
+
+const Teams = () => {
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <div>
+        <h1 className="text-2xl my-2">Team Page</h1>
+        <p>Welcome to our team page!</p>
+      </div>
+      <div>this is div 2</div>
+      <h1>heading</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default Teams;
